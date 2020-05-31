5f02334853de3becb6022b559f994cc4ebe4121a
PhiR42 <philippe.r@oult.eu>
2020-05-31 02:59:45 +0200

    setup & new version

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ from cx_Freeze import setup, Executable
 import version
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["six", "pkg_resources._vendor", "sysconfig"], "excludes": []}
+build_exe_options = {
+    "packages": ["six", "pkg_resources._vendor", "sysconfig"], 
+    "excludes": [],
+    'include_files' : ['textures_annotations.xml', ],
+    }
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -14,23 +18,12 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 APP = ['spacehaven-modloader.py']
-DATA_FILES = []
+DATA_FILES = [('spacehaven-modloader', ['textures_annotations.xml', ]), ]
 OPTIONS = {}
 
 setup(
     name="spacehaven-modloader",
     version=version.version,
-    app=APP,
-    data_files=DATA_FILES,
     options={"build_exe": build_exe_options},
     executables=[Executable("spacehaven-modloader.py", base=base)],
-    setup_requires=[],
-    install_requires=[
-        'lxml',
-        'cx_Freeze'
-        'click',
-        'pypng',
-        'six',
-        'appdirs'
-    ]
 )
