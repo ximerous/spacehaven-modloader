2c1a36eedcccc2673cf5cc604d3341b58335e183
Tahvohck <Tahvohck@gmail.com>
2020-12-02 01:35:57 -0500

    Activate enviroment before building

--- tools/build-windows.sh
+++ tools/build-windows.sh
@@ -3,7 +3,7 @@
 VERSION=`python -c 'import version; print(version.version)'`
 
 rm -rf build/* dist/*
-
+source env/Scripts/activate
 rm -rf dist/spacehaven-modloader dist/spacehaven-modloader-$VERSION.windows
 pyinstaller spacehaven-modloader.py --noconsole
 
