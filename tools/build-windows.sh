035f14ebeba416cf9890468631d4fe1e489a65cb
Tahvohck <Tahvohck@gmail.com>
2020-12-03 12:53:47 -0500

    Update build script to fix PATH clobbering from env
    - Also prevents previous builds from being erased if versions are different

--- tools/build-windows.sh
+++ tools/build-windows.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+OLDPATH=$PATH
+source env/Scripts/activate
 VERSION=`python -c 'import version; print(version.version)'`
 
-rm -rf build/* dist/*
+PATH=$OLDPATH
+rm -rf build/* dist/spacehaven-modloader dist/spacehaven-modloader-$VERSION.windows
 
-source env/Scripts/activate
-rm -rf dist/spacehaven-modloader dist/spacehaven-modloader-$VERSION.windows
 python -m PyInstaller --noconsole spacehaven-modloader.spec
+deactivate
 
+PATH=$OLDPATH
 mv dist/spacehaven-modloader dist/spacehaven-modloader-$VERSION.windows
 
 echo "-- Press enter key to continue --"
