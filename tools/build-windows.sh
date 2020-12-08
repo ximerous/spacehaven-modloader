e2bf1b4bbfb33be056bd4a3ec39a0ba93ebe544c
Tahvohck <Tahvohck@gmail.com>
2020-12-08 18:06:38 -0500

    Reorder build operations
    Don't delete build directory
    Get version after build
    Remove version-specific folder after build

--- tools/build-windows.sh
+++ tools/build-windows.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
+rm -rf dist/spacehaven-modloader
 
-OLDPATH=$PATH
 source env/Scripts/activate
-VERSION=`python -c 'import version; print(version.version)'`
-
-deactivate
-rm -rf build/* dist/spacehaven-modloader dist/spacehaven-modloader-$VERSION.windows
-source env/Scripts/activate
-
 python -m PyInstaller --noconsole modloader.spec
+VERSION=`python -c 'import version; print(version.version)'`
 deactivate
 
-PATH=$OLDPATH
+rm -rf dist/spacehaven-modloader-$VERSION.windows
 mv dist/spacehaven-modloader dist/spacehaven-modloader-$VERSION.windows
 
 echo "-- Press enter key to continue --"
