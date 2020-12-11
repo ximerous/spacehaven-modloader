dd1b5a5260f780c566b1cdcb7fb6d926b5c25138
Tahvohck <Tahvohck@gmail.com>
2020-12-11 17:16:27 -0500

    Add new metadata: Mod ID prefix

--- ui/database.py
+++ ui/database.py
@@ -94,6 +94,7 @@ class Mod:
             self.author = _optional("author")
             self.website = _optional("website")
             self.updates = _optional("updates")
+            self.prefix = int(_optional("modPrefix") or "0")
             
             self.verifyLoaderVersion(mod)
             self.verifyGameVersion(mod, self.gameInfo)
